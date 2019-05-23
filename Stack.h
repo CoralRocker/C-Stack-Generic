6a8ff397df3fe28d524befa4b20f707f0f397868
Gaultier Delbarre <gaultiertexas@gmail.com>
2019-05-23 12:21:34 -0500

    Made generic

--- Stack.h
+++ Stack.h
@@ -3,7 +3,7 @@
 #include <string.h>
 
 typedef struct Stack {
-	int* _stack;
+	void** _stack;
 	long empty: 1;
 	long _size: 31;
 } Stack;
@@ -26,19 +26,19 @@ void freeStack(Stack *s)
 	free(s);
 }
 
-int topStack(Stack* s)
+void* topStack(Stack* s)
 {
 	return s->_stack[0];
 }	
 
-void pushStack(Stack* s, int val)
+void pushStack(Stack* s, void* val)
 {
-	int* tmp = (int*)malloc(sizeof(int)*s->_size);
-	memcpy(tmp, s->_stack, sizeof(int)*s->_size);
+	void** tmp = (void**)malloc(sizeof(void*)*s->_size);
+	memcpy(tmp, s->_stack, sizeof(void*)*s->_size);
 	free(s->_stack);
-	s->_stack = (int*)malloc(sizeof(int)*(s->_size+1));
+	s->_stack = (void**)malloc(sizeof(void*)*(s->_size+1));
 	s->_size++;
-	memcpy(s->_stack+1, tmp, sizeof(int)*(s->_size-1));
+	memcpy(s->_stack+1, tmp, sizeof(void*)*(s->_size-1));
 	free(tmp);
 	s->_stack[0] = val;
 	s->empty = 0;
@@ -50,11 +50,11 @@ void popStack(Stack* s)
 	s->_size--;
 	if(s->_size != 0)
 	{
-		int* tmp = (int*)malloc(sizeof(int)*s->_size);
-		memcpy(tmp, s->_stack+1, sizeof(int)*s->_size);
+		void** tmp = (void**)malloc(sizeof(void*)*s->_size);
+		memcpy(tmp, s->_stack+1, sizeof(void*)*s->_size);
 		free(s->_stack);
-		s->_stack = (int*)malloc(sizeof(int)*s->_size);
-		memcpy(s->_stack, tmp, sizeof(int)*s->_size);
+		s->_stack = (void**)malloc(sizeof(void*)*s->_size);
+		memcpy(s->_stack, tmp, sizeof(void*)*s->_size);
 		free(tmp);
 	}else{
 		s->empty = 0;
