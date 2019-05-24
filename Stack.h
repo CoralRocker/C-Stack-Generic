f0d4f4d666fccbdc5a6097e31c2bb88aaa699b62
Gaultier Delbarre <gaultiertexas@gmail.com>
2019-05-24 17:06:49 -0500

    Added method comments.

--- Stack.h
+++ Stack.h
@@ -2,12 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* Stack struct */
 typedef struct Stack {
 	void** _stack;
 	long empty: 1;
 	long _size: 31;
 } Stack;
 
+/* Initializes a Stack object with malloc. Thus, must be freed to prevent memory leaks. Sets all
+ * appropriate variables to zero or NULL, save for empty, which is set to 1.
+ */
 Stack* initStack()
 {
 	Stack *s = (Stack*)malloc(sizeof(Stack));
@@ -17,6 +21,8 @@ Stack* initStack()
 	return s;
 }
 
+/* Frees the array and the stack structs. Must be called if the stack is initialized with initStack.
+ */
 void freeStack(Stack *s)
 {
 	if(s->_size==0)
@@ -26,11 +32,14 @@ void freeStack(Stack *s)
 	free(s);
 }
 
+/* Returns the top of the stack */
 void* topStack(Stack* s)
 {
 	return s->_stack[0];
 }	
 
+/* Pushes a given value to a new element at the top of the stack
+ */
 void pushStack(Stack* s, void* val)
 {
 	void** tmp = (void**)malloc(sizeof(void*)*s->_size);
@@ -45,6 +54,8 @@ void pushStack(Stack* s, void* val)
 
 }
 
+/* Removes the item at the top of the stack from said stack
+ */
 void popStack(Stack* s)
 {
 	s->_size--;
@@ -61,4 +72,3 @@ void popStack(Stack* s)
 		free(s->_stack);
 	}
 }
-
